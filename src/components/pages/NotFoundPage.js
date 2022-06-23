0139adc96ca4d83f19d3268281b7c5de6fad7ad4
Erin David <erindavid001@gmail.com>
2022-06-23 15:39:34 -0700

    create remaining page components

---
+++ src/components/pages/NotFoundPage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function NotFoundPage() {
+  return (
+    <div>Thats a bummer! looks like we don't have what you are looking for ;{'('}</div>
+  )
+}
+
+export default NotFoundPage
--- src/router/App.js
+++ src/router/App.js
@@ -4,7 +4,9 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from '../components/Header';
 import HomePage from "../components/pages/HomePage";
 import AboutPage from '../components/pages/AboutPage';
-
+import DevPage from '../components/pages/DevPage';
+import ArtworkPage from '../components/pages/ArtworkPage';
+import NotFoundPage from '../components/pages/NotFoundPage';
 
 
 function App() {
@@ -14,6 +16,9 @@ function App() {
         <Routes>
           <Route path={'/'} element={<HomePage/>}/>
           <Route path={'/about'} element={<AboutPage/>}/>
+          <Route path={'/artworks'} element={<ArtworkPage/>}/>
+          <Route path={'/development'} element={<DevPage/>}/>
+          <Route path='*' element={<NotFoundPage/>}/>
         </Routes>
     </BrowserRouter>
   );
