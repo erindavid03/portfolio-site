d94a0e1e1d744a5a1b77663e3fb5259a12c62ac3
Erin David <erindavid001@gmail.com>
2022-06-23 15:10:29 -0700

    test rest API

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,8 +1,28 @@
 import React from 'react'
+import {useState, useEffect} from 'react';
 
 function HomePage() {
+    const restPath = 'http://localhost/portfolio-project/wp-json/wp/v2/pages/8?_embed';
+    const [restData, setData] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetch(restPath)
+            if ( response.ok ) {
+                const data = await response.json()
+                setData(data)
+            } 
+        }
+        fetchData()
+    }, [restPath])
+
+    console.log(restData.content);
+
   return (
-    <div>HomePage</div>
+    <div>
+        <h1>hello</h1>
+        <div className="entry-content" dangerouslySetInnerHTML={{__html:restData.content.rendered}}></div>
+    </div>
   )
 }
 
--- src/router/App.js
+++ src/router/App.js
@@ -3,7 +3,7 @@ import HomePage from "../components/HomePage";
 
 function App() {
   return (
-    <HomePage />
+    <HomePage/>
   );
 }
 
