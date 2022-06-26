ebf84a935c77ecd4ec4666fffc04b99f697e29e8
Erin David <erindavid001@gmail.com>
2022-06-25 19:15:11 -0700

    add artwork page content

--- src/components/pages/ArtworkPage.js
+++ src/components/pages/ArtworkPage.js
@@ -1,8 +1,25 @@
-import React from 'react'
+import React from 'react';
+import {Link} from 'react-router-dom';
+// import images here
+import ArtBanner from '../images/art-banner.png';
 
 function ArtworkPage() {
   return (
-    <div>ArtworkPage</div>
+    <div>
+      <div>
+        <h1>Artworks</h1>
+        <p>This is where I keep all my cool artwork! I enjoy both digital and traditional media as a means to produce neat projects.  I take inspiration from old anime, psychological horror, and Wes Anderson movies. 
+        I mostly use AdobePhotoshhop and FireAlpaca when drawing digitally, while my favourite traditional tool is shading pencils and acrylic paint!</p>
+
+        <img src={`${ArtBanner}`} alt="Image of Cat drawing" />
+      </div>
+
+    
+
+      <div>
+
+      </div>
+    </div>
   )
 }
 
