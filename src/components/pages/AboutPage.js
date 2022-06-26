90f21bf7288380f2b2c45102916d56fa6032b1e0
Erin David <erindavid001@gmail.com>
2022-06-26 00:44:41 -0700

    add about page content

--- src/components/pages/AboutPage.js
+++ src/components/pages/AboutPage.js
@@ -2,7 +2,17 @@ import React from 'react'
 
 function AboutPage() {
   return (
-    <div>AboutPage</div>
+    <div>
+      <h1>About Me</h1>
+      <h2>Hi there! Im Erin {':)'}</h2>
+      <p>I am a Front-End Web Developer from the lower mainland with a passion for designing original creations. Whether it be a website or an art piece, I ensure that all my work is thoughtful, creative, and polished!
+
+Since I grew up around the time that the internet was in its heyday, a lot of my inspiration comes from early 2000s nostalgia and modern-day aesthetics. After gaining an in-depth understanding of the best practices in UX/UI Design and Web Development, it is a personal goal for me to create sites that are pleasing to look at, and take you back to the joys of growing up with the internet!
+
+When I am not around my computer, I enjoy doodling away in my sketchbooks while spinning my favourite records from my collection! Either that or I’m plotting my next plan for world domination with my army of burnt toast {'>:D'}
+
+ </p>
+    </div>
   )
 }
 
