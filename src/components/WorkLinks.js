44432de7b3a33482ab25ca934ea964b8963cebd9
Erin David <erindavid001@gmail.com>
2022-06-25 19:03:06 -0700

    create icon changes on hover

--- src/components/WorkLinks.js
+++ src/components/WorkLinks.js
@@ -1,24 +1,35 @@
 import React from 'react';
 // insert react stuff here
 import {Link} from 'react-router-dom';
+import { useState } from 'react';
 // insert any images here
 import ArtworkIcon from '../components/images/icons/art-icon.png';
+import ArtworkHover from '../components/images/icons/art-icon-hover.gif';
 import DevIcon from '../components/images/icons/dev-icon.png'
+import DevHover from '../components/images/icons/dev-icon-hover.gif';
 
 function WorkLinks() {
     
+    const [hoverCheck, setHoverCheck] = useState(false);
+    const [hoverCheckDev, setHoverCheckDev] = useState(false);
+
 
   return (
     <div className='work-links'>
-        <Link to='/artworks'>
+        <Link to='/artworks'
+        onMouseOver={()=>setHoverCheck(true)}
+        onMouseOut={()=>setHoverCheck(false)}>
             <article>
-                <img src={`${ArtworkIcon}`} alt="Cat holding a piece of artwork"/>
+                <img 
+                 src={hoverCheck ? `${ArtworkHover}` : `${ArtworkIcon}`} alt="Cat holding a piece of artwork"/>
                 <h3>Artworks</h3>
             </article>
         </Link>  
-        <Link to='/development'>
+        <Link to='/development'
+        onMouseOver={()=>setHoverCheckDev(true)}
+        onMouseOut={()=>setHoverCheckDev(false)}>
             <article>
-                <img src={`${DevIcon}`} alt="Cat holding a piece of artwork"/>
+                <img src={hoverCheckDev ? `${DevHover}` : `${DevIcon}`} alt="Cat holding a piece of artwork"/>
                 <h3>Artworks</h3>
             </article>
         </Link>  
