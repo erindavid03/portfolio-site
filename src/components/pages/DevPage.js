1530a84434710354e88d57a3facabbf7f009e809
Erin David <erindavid001@gmail.com>
2022-06-25 19:30:46 -0700

    add dev page content

--- src/components/pages/DevPage.js
+++ src/components/pages/DevPage.js
@@ -1,8 +1,22 @@
-import React from 'react'
+import React from 'react';
+import DevBanner from '../images/dev-banner.png';
 
 function DevPage() {
   return (
-    <div>DevPage</div>
+    <div>
+      <div>
+        <h1>Development</h1>
+        
+        <p>These are all some of my development projects! I am quite versatile when it comes to what I do in development — I like both coding and design, but I lean towards design more. I take a lot of inspiration from old internet aesthetics and art when it comes to design!
+          In terms of where I like to develop, I like React the most, but I also dabble on WordPress development {'(❁´◡`❁)'}</p>
+
+        <img src={`${DevBanner}`} alt="an Image of a cat coding" />
+      </div>
+
+      <div>
+        
+      </div>
+    </div>
   )
 }
 
