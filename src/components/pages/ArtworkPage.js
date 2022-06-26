ff299d0e3e4dccfeb5a9a9b4f1643f5aa1f513d4
Erin David <erindavid001@gmail.com>
2022-06-26 13:07:49 -0700

    begin trial api call to portfolio WP site content

--- src/components/pages/ArtworkPage.js
+++ src/components/pages/ArtworkPage.js
@@ -1,10 +1,36 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import {useState, useEffect} from 'react';
 // import images here
 import ArtBanner from '../images/art-banner.png';
 
 function ArtworkPage() {
+
+  const apiPath = 'https://erindee.ca/erin-portfolio/wp-json/wp/v2/portfolio-artworks';
+  const [restData, setRestData] = useState([]);
+  const [isLoaded, setLoad] = useState(false);
+
+  useEffect( () =>{
+    const fetchData = async () =>{
+      const response = await fetch (apiPath);
+      if(response.ok){
+        const data = await response.json();
+        setRestData(data);
+        setLoad(true);
+      } else{
+        setLoad(false);
+      }
+    }
+    fetchData();
+  }, [apiPath]) 
+
+if(isLoaded === false){
+  console.log('react u failed me');
+} else{
+  console.log('yay! it worked!')
+}
   return (
+
     <div>
       <div>
         <h1>Artworks</h1>
@@ -13,10 +39,13 @@ function ArtworkPage() {
 
         <img src={`${ArtBanner}`} alt="Image of Cat drawing" />
       </div>
-
-    
-
+  
       <div>
+        {isLoaded === true ?
+
+          restData.map(artwork => <h2 key={artwork.id}>{artwork.title.rendered}</h2>) :
+          <p>Loading...</p>
+        }
 
       </div>
     </div>
