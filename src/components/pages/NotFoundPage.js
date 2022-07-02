af6318d5346a2799d1f0a65a6bbd0574a72a6062
Erin David <erindavid001@gmail.com>
2022-07-02 14:06:47 -0700

    finalize 404 page content

--- src/components/pages/NotFoundPage.js
+++ src/components/pages/NotFoundPage.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import WorkLinks from '../WorkLinks'
 
 function NotFoundPage() {
   return (
-    <div>Thats a bummer! looks like we don't have what you are looking for ;{'('}</div>
+    <section>
+      <h1>Whats this? An empty room!</h1>
+      <p>Thats a bummer! looks like we don't have what you are looking for ;{'('} unless, of course, you were looking for nothing the whole time!
+          In that case, congratulations! You've found nothing :D</p>
+
+      <h2>Maybe try looking here for something super awesome {'(:'}</h2>
+      <WorkLinks/>
+      
+      </section>
   )
 }
 
