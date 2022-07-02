bec075c3d74d438ea74c02d55dae5c706baad4c7
Erin David <erindavid001@gmail.com>
2022-07-02 14:00:16 -0700

    add list of skills in about page

--- src/components/pages/AboutPage.js
+++ src/components/pages/AboutPage.js
@@ -2,17 +2,45 @@ import React from 'react'
 
 function AboutPage() {
   return (
-    <div>
+  <>
+    <section>
       <h1>About Me</h1>
       <h2>Hi there! Im Erin {':)'}</h2>
       <p>I am a Front-End Web Developer from the lower mainland with a passion for designing original creations. Whether it be a website or an art piece, I ensure that all my work is thoughtful, creative, and polished!
+        Since I grew up around the time that the internet was in its heyday, a lot of my inspiration comes from early 2000s nostalgia and modern-day aesthetics. After gaining an in-depth understanding of the best practices in UX/UI Design and Web Development, it is a personal goal for me to create sites that are pleasing to look at, and take you back to the joys of growing up with the internet!
+        When I am not around my computer, I enjoy doodling away in my sketchbooks while spinning my favourite records from my collection! Either that or I’m plotting my next plan for world domination with my army of burnt toast {'>:D'}
+      </p>
+    </section>
 
-Since I grew up around the time that the internet was in its heyday, a lot of my inspiration comes from early 2000s nostalgia and modern-day aesthetics. After gaining an in-depth understanding of the best practices in UX/UI Design and Web Development, it is a personal goal for me to create sites that are pleasing to look at, and take you back to the joys of growing up with the internet!
+    <section>
+      <h2>My skills</h2>
+      <h3>Development</h3>
+      <ul>
+        <li>React</li>
+        <li>PHP</li>
+        <li>WordPress</li>
+        <li>JavaScript</li>
+        <li>HTML</li>
+        <li>CSS/SASS</li>
+        <li>jQuery</li>
+      </ul>
 
-When I am not around my computer, I enjoy doodling away in my sketchbooks while spinning my favourite records from my collection! Either that or I’m plotting my next plan for world domination with my army of burnt toast {'>:D'}
+      <h3>Design</h3>
+        <ul>
+          <li>Adobe XD</li>
+          <li>Figma</li>
+          <li>Adobe Illustrator</li>
+          <li>InVision</li>
+        </ul>
 
- </p>
-    </div>
+      <h3>Art</h3>
+      <ul>
+        <li>Adobe Photoshop</li>
+        <li>Adobe Premiere</li>
+        <li>Adobe Animate</li>
+      </ul>
+    </section>
+  </>
   )
 }
 
