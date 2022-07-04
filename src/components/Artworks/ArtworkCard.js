fd931cd6b2463f36945021df29f77afc6d80e100
Erin David <erindavid001@gmail.com>
2022-07-03 17:48:59 -0700

    match html layout of devcard in artwork card

--- src/components/Artworks/ArtworkCard.js
+++ src/components/Artworks/ArtworkCard.js
@@ -1,15 +1,27 @@
 import React from 'react'
 import {Link} from 'react-router-dom';
+import { useState, useEffect } from 'react';
+
 
 function ArtworkCard({artwork}) {
+  const [isWide, setIsWide] = useState(
+    window.matchMedia("(min-width: 64em)").matches
+    );
+
+    useEffect(() => {
+      window
+      .matchMedia("(min-width: 64em)")
+      .addEventListener('change', e => setIsWide( e.matches ));
+    }, []);
+
   return (
-    <article>
+    <article className={isWide ? 'card' : 'card slide'}>
         <div>
             <img src={artwork.acf.artwork_image}/> 
-            <h2>{artwork.title.rendered}</h2>
         </div>
+        <Link to={`/artworks/${artwork.id}`}>{artwork.title.rendered}</Link>
 
-        <div>
+        <div className='work-blurb'>
             <p>{artwork.acf.general_statement}</p>
             <Link to={`/artworks/${artwork.id}`}>More Info</Link>
         </div>
