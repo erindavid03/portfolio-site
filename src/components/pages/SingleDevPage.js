b2191f74c822a52699f6b9f1759abaa3cb71d6ba
Erin David <erindavid001@gmail.com>
2022-07-04 20:05:43 -0700

    contain in progress pictures in validating if statements

--- src/components/pages/SingleDevPage.js
+++ src/components/pages/SingleDevPage.js
@@ -54,18 +54,20 @@ function SingleDevPage() {
                 <TabList>
                 <Tab>About This Piece</Tab>
                 <Tab>Reflection</Tab>
-                <Tab>In Progress Pictures</Tab>
+                {restData.acf.wip_pictures && <Tab>In Progress Pictures</Tab>}
                 </TabList>
 
                 <TabPanel>
                 <h2>About 'TITLE'</h2>
                 <p>This is stuff about the piece. Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis quasi explicabo fuga ex. Repudiandae, itaque quae cum suscipit atque perspiciatis. Fugit iure aut molestias aliquam odio dolorum quidem ducimus ab?</p>
                 </TabPanel>
+
                 <TabPanel>
                 <h2>Reflection</h2>
                 <p>This is a reflection. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tenetur officia ut, ullam cupiditate eos velit? Illo est aspernatur facilis qui consectetur necessitatibus inventore cum eaque, alias atque optio cupiditate animi?</p>
                 </TabPanel>
-                <TabPanel>
+
+                {restData.acf.wip_pictures && <TabPanel>
                 <h2>In Progress</h2>
                 <p>Here are pictures during the progress</p>
                 <div>
@@ -78,8 +80,7 @@ function SingleDevPage() {
                     <img src='https://i.kym-cdn.com/photos/images/newsfeed/002/205/307/1f7.jpg' />
                     <img src='https://i.kym-cdn.com/photos/images/newsfeed/002/205/307/1f7.jpg' />
                 </div>
-
-                </TabPanel>
+                </TabPanel>}
             </Tabs>
         </section> 
         </> :
