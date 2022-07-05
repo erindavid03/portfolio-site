e2d7f02410b08ddb8c96477661d61bc5bcdf31ad
Erin David <erindavid001@gmail.com>
2022-07-05 10:34:10 -0700

    create key features component

---
+++ src/components/Dev/KeyFeatures.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function KeyFeatures({feature}) {
+  return (
+    <div>
+        <h3>Feature Title</h3>
+        <p>put stuff about feature here</p>
+        <img src="" alt="" />
+    </div>
+  )
+}
+
+export default KeyFeatures
