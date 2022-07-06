81d4eb086e624566f0b29eed1d13ecf7ea9843d8
Erin David <erindavid001@gmail.com>
2022-07-06 11:15:34 -0700

    wrap about page in div

--- src/components/pages/AboutPage.js
+++ src/components/pages/AboutPage.js
@@ -4,44 +4,46 @@ import HeroImage from '../images/me-picture.png';
 function AboutPage() {
   return (
   <>
-    <section>
-      <h1>About Me</h1>
-      <img src={HeroImage} alt="" />
-      <h2>Hi there! Im Erin {':)'}</h2>
-      <p>I am a Front-End Web Developer from the lower mainland with a passion for designing original creations. Whether it be a website or an art piece, I ensure that all my work is thoughtful, creative, and polished!
-        Since I grew up around the time that the internet was in its heyday, a lot of my inspiration comes from early 2000s nostalgia and modern-day aesthetics. After gaining an in-depth understanding of the best practices in UX/UI Design and Web Development, it is a personal goal for me to create sites that are pleasing to look at, and take you back to the joys of growing up with the internet!
-        When I am not around my computer, I enjoy doodling away in my sketchbooks while spinning my favourite records from my collection! Either that or I’m plotting my next plan for world domination with my army of burnt toast {'>:D'}
-      </p>
-    </section>
+    <div>
+      <section>
+        <h1>About Me</h1>
+        <img src={HeroImage} alt="an image of a girl holding a cat" />
+        <h2>Hi there! Im Erin {':)'}</h2>
+        <p>I am a Front-End Web Developer from the lower mainland with a passion for designing original creations. Whether it be a website or an art piece, I ensure that all my work is thoughtful, creative, and polished!
+          Since I grew up around the time that the internet was in its heyday, a lot of my inspiration comes from early 2000s nostalgia and modern-day aesthetics. After gaining an in-depth understanding of the best practices in UX/UI Design and Web Development, it is a personal goal for me to create sites that are pleasing to look at, and take you back to the joys of growing up with the internet!
+          When I am not around my computer, I enjoy doodling away in my sketchbooks while spinning my favourite records from my collection! Either that or I’m plotting my next plan for world domination with my army of burnt toast {'>:D'}
+        </p>
+      </section>
 
-    <section>
-      <h2>My skills</h2>
-      <h3>Development</h3>
-      <ul>
-        <li>React</li>
-        <li>PHP</li>
-        <li>WordPress</li>
-        <li>JavaScript</li>
-        <li>HTML</li>
-        <li>CSS/SASS</li>
-        <li>jQuery</li>
-      </ul>
-
-      <h3>Design</h3>
+      <section>
+        <h2>My skills</h2>
+        <h3>Development</h3>
         <ul>
-          <li>Adobe XD</li>
-          <li>Figma</li>
-          <li>Adobe Illustrator</li>
-          <li>InVision</li>
+          <li>React</li>
+          <li>PHP</li>
+          <li>WordPress</li>
+          <li>JavaScript</li>
+          <li>HTML</li>
+          <li>CSS/SASS</li>
+          <li>jQuery</li>
         </ul>
 
-      <h3>Art</h3>
-      <ul>
-        <li>Adobe Photoshop</li>
-        <li>Adobe Premiere</li>
-        <li>Adobe Animate</li>
-      </ul>
-    </section>
+        <h3>Design</h3>
+          <ul>
+            <li>Adobe XD</li>
+            <li>Figma</li>
+            <li>Adobe Illustrator</li>
+            <li>InVision</li>
+          </ul>
+
+        <h3>Art</h3>
+        <ul>
+          <li>Adobe Photoshop</li>
+          <li>Adobe Premiere</li>
+          <li>Adobe Animate</li>
+        </ul>
+      </section>
+    </div>
   </>
   )
 }
