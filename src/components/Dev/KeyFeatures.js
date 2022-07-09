90e32f98c755126db69f8d40a4c991ba9b2f474e
Erin David <erindavid001@gmail.com>
2022-07-08 20:08:15 -0700

    output featured images in key features

--- src/components/Dev/KeyFeatures.js
+++ src/components/Dev/KeyFeatures.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 function KeyFeatures( {feature} ) {
   return (
-    <article>
+    <article className='feature-grid'>
       <div>
         <h3>{feature.feature_title}</h3>
         <p>{feature.feature_details}</p>
       </div>
-       {feature.feature_clip && <img src={`${feature.feature_clip}`} alt =''/>}
+       {feature.feature_clip && <img src={`${feature.feature_clip.url}`} alt ={feature.feature_clip.alt}/>}
     </article>
   )
 }
